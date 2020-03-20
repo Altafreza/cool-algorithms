4b29f0fd33e0414ffffc49dd0ba72a418fd62a02
Rakuten <muddassirzfr95@gmail.com>
2020-03-20 21:30:59 +0530

    word break

--- leetcode/src/dynamicprograming/WordBreak.java
+++ leetcode/src/dynamicprograming/WordBreak.java
@@ -3,15 +3,15 @@ package dynamicprograming;
 import java.util.*;
 
 public class WordBreak {
-    public int wordBreak(String s, ArrayList<String> dict) {
+    public static boolean wordBreak(String s, ArrayList<String> dict) {
         HashSet<String> set = new HashSet<>(dict);
         // Wrapper required because if not it is always false which is an answer
         // here 3 values are there null, false true;
         Boolean[] dp = new Boolean[s.length()];
-        return helper(0, set, s, dp) ? 1 : 0;
+        return helper(0, set, s, dp);
     }
 
-    public boolean helper(int idx, HashSet<String> dict, String s, Boolean[] dp) {
+    public static boolean helper(int idx, HashSet<String> dict, String s, Boolean[] dp) {
         if (dict.contains(s))
             return true;
         if (dp[idx] != null)
@@ -19,7 +19,7 @@ public class WordBreak {
 
         for (int i = 0; i < s.length(); i++) {
             String sub = s.substring(0, i + 1);
-            if (dict.contains(sub) && helper(i + 1, dict, s.substring(i + 1), dp)) {
+            if (dict.contains(sub) && helper(idx + i+1, dict, s.substring(i + 1), dp)) {
                 dp[idx] = true;
                 return true;
             }
@@ -28,93 +28,106 @@ public class WordBreak {
         return false;
     }
 
-    class Solution {
-        public boolean wordBreak90(String s, List<String> wordDict) {
-            // assume s and wordDict are non-empty
-            int n = s.length();
-            Boolean[] memo = new Boolean[n]; // memo[i] --> S[i...] is breakable or not
-            return backtracking(0, s, new HashSet<String>(wordDict), memo);
+    public static boolean wordBreak90(String s, List<String> wordDict) {
+        // assume s and wordDict are non-empty
+        int n = s.length();
+        Boolean[] memo = new Boolean[n]; // memo[i] --> S[i...] is breakable or not
+        return backtracking(0, s, new HashSet<String>(wordDict), memo);
+    }
+
+    private static boolean backtracking(int depth, String s, Set<String> wordSet, Boolean[] memo) {
+        int n = s.length();
+        // accept
+        if (depth == n) {
+            return true;
+        }
+        // memoization
+        if (memo[depth] != null) { // memo
+            return memo[depth];
         }
 
-        private boolean backtracking(int depth, String s, Set<String> wordSet, Boolean[] memo) {
-            int n = s.length();
-            // accept
-            if (depth == n) {
+        for (int i = depth; i < n; ++i) {
+            String str = s.substring(depth, i + 1); // substring[depth, i]
+            if (wordSet.contains(str) && backtracking(i + 1, s, wordSet, memo)) {
+                memo[depth] = true; // memo
                 return true;
             }
-            // memoization
-            if (memo[depth] != null) { // memo
-                return memo[depth];
-            }
+        }
 
-            for (int i = depth; i < n; ++i) {
-                String str = s.substring(depth, i + 1); // substring[depth, i]
-                if (wordSet.contains(str) && backtracking(i + 1, s, wordSet, memo)) {
-                    memo[depth] = true; // memo
-                    return true;
+        memo[depth] = false; // memo
+        return false;
+    }
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet(wordDict);
+        HashMap<String, Boolean> dp = new HashMap<String, Boolean>();
+        // return helper(s, dict, dp);
+        return dpWB(s, dict);
+    }
+
+    public static boolean wordBreak1(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                String sub = s.substring(j, i);
+                if (set.contains(sub) && dp[j])
+                    dp[i] = true;
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    public static boolean dpWB(String s, Set<String> dict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[s.length()] = true;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j <= n; j++) {
+                String sub = s.substring(i, j);
+                if (dict.contains(sub) && dp[j]) {
+                    dp[i] = true;
+                    break;
                 }
             }
+        }
+        return dp[0];
+    }
 
-            memo[depth] = false; // memo
-            return false;
+    public static boolean helper(String s, Set<String> dict, HashMap<String, Boolean> dp) {
+        if (s.length() == 0)
+            return true;
+        if (dp.containsKey(s)) {
+            return dp.get(s);
         }
 
-        public boolean wordBreak(String s, List<String> wordDict) {
-            Set<String> dict = new HashSet(wordDict);
-            HashMap<String, Boolean> dp = new HashMap<String, Boolean>();
-            // return helper(s, dict, dp);
-            return dpWB(s, dict);
-        }
-
-        public boolean wordBreak1(String s, List<String> wordDict) {
-            HashSet<String> set = new HashSet<>(wordDict);
-            boolean[] dp = new boolean[s.length() + 1];
-            dp[0] = true;
-            for (int i = 0; i <= s.length(); i++) {
-                for (int j = 0; j < i; j++) {
-                    String sub = s.substring(j, i);
-                    if (set.contains(sub) && dp[j])
-                        dp[i] = true;
-                }
-            }
-
-            return dp[s.length()];
-        }
-
-        public boolean dpWB(String s, Set<String> dict) {
-            int n = s.length();
-            boolean[] dp = new boolean[n + 1];
-            dp[s.length()] = true;
-            for (int i = n - 1; i >= 0; i--) {
-                for (int j = i + 1; j <= n; j++) {
-                    String sub = s.substring(i, j);
-                    if (dict.contains(sub) && dp[j]) {
-                        dp[i] = true;
-                        break;
-                    }
-                }
-            }
-            return dp[0];
-        }
-
-        public boolean helper(String s, Set<String> dict, HashMap<String, Boolean> dp) {
-            if (s.length() == 0)
+        for (int i = 0; i < s.length(); i++) {
+            String left = s.substring(0, i);
+            String right = s.substring(i);
+            if (dict.contains(right) && helper(left, dict, dp)) {
+                dp.put(s, true);
                 return true;
-            if (dp.containsKey(s)) {
-                return dp.get(s);
             }
-
-            for (int i = 0; i < s.length(); i++) {
-                String left = s.substring(0, i);
-                String right = s.substring(i);
-                if (dict.contains(right) && helper(left, dict, dp)) {
-                    dp.put(s, true);
-                    return true;
-                }
-            }
-            dp.put(s, false);
-            return false;
         }
+        dp.put(s, false);
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        /*System.out.println(wordBreak("catsanddogsk", //true
+             new ArrayList<String>(Arrays.asList("cats", "and", "dogs","k"))));
+        System.out.println(wordBreak("aaaaaab",
+                new ArrayList<String>(Arrays.asList("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa",
+                        "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"))));*/
+        System.out.println(wordBreak("abbcdd",
+                new ArrayList<String>(Arrays.asList("a", "bb","ab", "abc", "cd", "d"))));
+
+       /* System.out.println(wordBreak("fohhemkkaecojceoaejkkoedkofhmohkcjmkggcmnami", //true
+                new ArrayList<String>(Arrays.asList("kfomka","hecagbngambii","anobmnikj","c","nnkmfelneemfgcl","ah","bgomgohl","lcbjbg","ebjfoiddndih","hjknoamjbfhckb","eioldlijmmla","nbekmcnakif","fgahmihodolmhbi","gnjfe","hk","b","jbfgm","ecojceoaejkkoed","cemodhmbcmgl","j","gdcnjj","kolaijoicbc","liibjjcini","lmbenj","eklingemgdjncaa","m","hkh","fblb","fk","nnfkfanaga","eldjml","iejn","gbmjfdooeeko","jafogijka","ngnfggojmhclkjd","bfagnfclg","imkeobcdidiifbm","ogeo","gicjog","cjnibenelm","ogoloc","edciifkaff","kbeeg","nebn","jdd","aeojhclmdn","dilbhl","dkk","bgmck","ohgkefkadonafg","labem","fheoglj","gkcanacfjfhogjc","eglkcddd","lelelihakeh","hhjijfiodfi","enehbibnhfjd","gkm","ggj","ag","hhhjogk","lllicdhihn","goakjjnk","lhbn","fhheedadamlnedh","bin","cl","ggjljjjf","fdcdaobhlhgj","nijlf","i","gaemagobjfc","dg","g","jhlelodgeekj","hcimohlni","fdoiohikhacgb","k","doiaigclm","bdfaoncbhfkdbjd","f","jaikbciac","cjgadmfoodmba","molokllh","gfkngeebnggo","lahd","n","ehfngoc","lejfcee","kofhmoh","cgda","de","kljnicikjeh","edomdbibhif","jehdkgmmofihdi","hifcjkloebel","gcghgbemjege","kobhhefbbb","aaikgaolhllhlm","akg","kmmikgkhnn","dnamfhaf","mjhj","ifadcgmgjaa","acnjehgkflgkd","bjj","maihjn","ojakklhl","ign","jhd","kndkhbebgh","amljjfeahcdlfdg","fnboolobch","gcclgcoaojc","kfokbbkllmcd","fec","dljma","noa","cfjie","fohhemkka","bfaldajf","nbk","kmbnjoalnhki","ccieabbnlhbjmj","nmacelialookal","hdlefnbmgklo","bfbblofk","doohocnadd","klmed","e","hkkcmbljlojkghm","jjiadlgf","ogadjhambjikce","bglghjndlk","gackokkbhj","oofohdogb","leiolllnjj","edekdnibja","gjhglilocif","ccfnfjalchc","gl","ihee","cfgccdmecem","mdmcdgjelhgk","laboglchdhbk","ajmiim","cebhalkngloae","hgohednmkahdi","ddiecjnkmgbbei","ajaengmcdlbk","kgg","ndchkjdn","heklaamafiomea","ehg","imelcifnhkae","hcgadilb","elndjcodnhcc","nkjd","gjnfkogkjeobo","eolega","lm","jddfkfbbbhia","cddmfeckheeo","bfnmaalmjdb","fbcg","ko","mojfj","kk","bbljjnnikdhg","l","calbc","mkekn","ejlhdk","hkebdiebecf","emhelbbda","mlba","ckjmih","odfacclfl","lgfjjbgookmnoe","begnkogf","gakojeblk","bfflcmdko","cfdclljcg","ho","fo","acmi","oemknmffgcio","mlkhk","kfhkndmdojhidg","ckfcibmnikn","dgoecamdliaeeoa","ocealkbbec","kbmmihb","ncikad","hi","nccjbnldneijc","hgiccigeehmdl","dlfmjhmioa","kmff","gfhkd","okiamg","ekdbamm","fc","neg","cfmo","ccgahikbbl","khhoc","elbg","cbghbacjbfm","jkagbmfgemjfg","ijceidhhajmja","imibemhdg","ja","idkfd","ndogdkjjkf","fhic","ooajkki","fdnjhh","ba","jdlnidngkfffbmi","jddjfnnjoidcnm","kghljjikbacd","idllbbn","d","mgkajbnjedeiee","fbllleanknmoomb","lom","kofjmmjm","mcdlbglonin","gcnboanh","fggii","fdkbmic","bbiln","cdjcjhonjgiagkb","kooenbeoongcle","cecnlfbaanckdkj","fejlmog","fanekdneoaammb","maojbcegdamn","bcmanmjdeabdo","amloj","adgoej","jh","fhf","cogdljlgek","o","joeiajlioggj","oncal","lbgg","elainnbffk","hbdi","femcanllndoh","ke","hmib","nagfahhljh","ibifdlfeechcbal","knec","oegfcghlgalcnno","abiefmjldmln","mlfglgni","jkofhjeb","ifjbneblfldjel","nahhcimkjhjgb","cdgkbn","nnklfbeecgedie","gmllmjbodhgllc","hogollongjo","fmoinacebll","fkngbganmh","jgdblmhlmfij","fkkdjknahamcfb","aieakdokibj","hddlcdiailhd","iajhmg","jenocgo","embdib","dghbmljjogka","bahcggjgmlf","fb","jldkcfom","mfi","kdkke","odhbl","jin","kcjmkggcmnami","kofig","bid","ohnohi","fcbojdgoaoa","dj","ifkbmbod","dhdedohlghk","nmkeakohicfdjf","ahbifnnoaldgbj","egldeibiinoac","iehfhjjjmil","bmeimi","ombngooicknel","lfdkngobmik","ifjcjkfnmgjcnmi","fmf","aoeaa","an","ffgddcjblehhggo","hijfdcchdilcl","hacbaamkhblnkk","najefebghcbkjfl","hcnnlogjfmmjcma","njgcogemlnohl","ihejh","ej","ofn","ggcklj","omah","hg","obk","giig","cklna","lihaiollfnem","ionlnlhjckf","cfdlijnmgjoebl","dloehimen","acggkacahfhkdne","iecd","gn","odgbnalk","ahfhcd","dghlag","bchfe","dldblmnbifnmlo","cffhbijal","dbddifnojfibha","mhh","cjjol","fed","bhcnf","ciiibbedklnnk","ikniooicmm","ejf","ammeennkcdgbjco","jmhmd",
+                        "cek","bjbhcmda","kfjmhbf","chjmmnea","ifccifn","naedmco","iohchafbega","kjejfhbco","anlhhhhg"))));*/
     }
 
 }
---
+++ leetcode/src/dynamicprograming/WordBreak2.java
@@ -0,0 +1,11 @@
+package dynamicprograming;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class WordBreak2 {
+    public static List<String> wordBreak(String s, List<String> wordDict){
+        //helper(0,s, new HashSet<String>(wordDict));
+        return null;
+    }
+}
